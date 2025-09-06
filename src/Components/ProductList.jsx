85bc2891b916b91bf6ff9492f7028bf7a02c6917
youssefshawwa1 <youssefshawwa1@gmail.com>
2025-09-06 02:49:54 -0400

    my commit

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './ProductList.css'; 
+import { useDispatch, useSelector } from 'react-redux';
+import { addItemToCart } from './CartSlice';// Action to add product to cart
 
 const ProductList = () => {
 
@@ -8,12 +10,27 @@ const ProductList = () => {
     { id: 2, name: 'Product B', price: 75 },
     { id: 3, name: 'Product C', price: 30 },
   ];
-
+  const dispatch = useDispatch();
+  const  cartItems = useSelector(state => state.cart.cartItems); // Get cart items globally
+  const handleAddToCart = product => {
+    dispatch(addItemToCart(product));// Add product to cart
+  };
   return (
     <div className="product-list">
       <h2 className="product-list-title">Products</h2>
       <ul className="product-list-items">
-     
+      {products.map(product => (
+            <li key={product.id} className="product-list-item">
+            <span>{product.name} - ${product.price}</span>
+            <button
+                className={`add-to-cart-btn ${cartItems.some(item => item.id === product.id) ? 'disabled' : ''}`}
+                onClick={() => handleAddToCart(product)}
+                disabled={cartItems.some(item => item.id === product.id)} // Disable if already in cart
+                >
+                {cartItems.some(item => item.id === product.id) ? 'Added' : 'Add to Cart'}
+            </button>
+            </li>
+        ))}
       </ul>
     </div>
   );
